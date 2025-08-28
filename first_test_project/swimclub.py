dd48e2d13cfe59d5c965d0991304371fd857df18
WedgeBeard <tysonstecklein@gmail.com>
2025-08-28 17:14:20 -0600

    completed chapter 3, updated files, added comments

--- first_test_project/swimclub.py
+++ first_test_project/swimclub.py
@@ -2,19 +2,28 @@ FOLDER = "swimdata/"
 import statistics
 
 def read_swim_data(filename):
+    """Return swim data from a file.
+    
+    Given the name of a swimmer's file (in filename), extract all the required
+    data, then return it to the caller as a tuple.
+    """
     swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
     with open(FOLDER + filename) as file:
         line = file.readlines()
         times = line[0].strip().split(",")
     converted = []
     for t in times:
-        minutes, rest = t.split(":")
+        # The swim data coule be less than 1 minute, guard against it here.
+        if ":" in t:
+            minutes, rest = t.split(":")
+        else:
+            minutes = 0
+            rest = t
         seconds, milliseconds = rest.split(".")
         converted.append((int(minutes) * 6000) + (int(seconds) * 100) + int(milliseconds))
-    sum = 0
     avg = statistics.mean(converted)
     avg_minutes = int(avg/6000)
     avg_seconds = int(int(avg - (6000*avg_minutes))/100)
     avg_milliseconds = round(avg - avg_minutes*6000 - avg_seconds * 100)
     avg_str = (str(avg_minutes) + ":" + str(avg_seconds) + "." + str(avg_milliseconds))
-    return swimmer, age, distance, stroke, times, avg_str
+    return swimmer, age, distance, stroke, times, avg_str # Returned as a tuple.
