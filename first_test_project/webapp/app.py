9ebcb8ee86f6279885838859e9171874e864a5b3
WedgeBeard <tysonstecklein@gmail.com>
2025-09-04 15:37:40 -0600

    Adding to the web app

--- first_test_project/webapp/app.py
+++ first_test_project/webapp/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import os
+import swimclub
 
 app = Flask(__name__)
+
 
 @app.get("/")
 def index():
@@ -8,3 +11,15 @@ def index():
 
 if __name__ == "__main__":
     app.run()
+
+
+@app.get("/swimmers")
+def display_swimmers():
+    swim_files = os.listdir(swimclub.FOLDER)
+    swimmers = {}
+    for f in swim_files:
+        name, *_ = swimclub.read_swim_data(f)
+        if name not in swimmers:
+            swimmers[name] = []
+        swimmers[name].append(f)
+    return str(sorted(swimmers))
