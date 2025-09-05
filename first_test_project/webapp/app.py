8facf4f303d69e89908b33ab57a8266e7d4a4d04
WedgeBeard <tysonstecklein@gmail.com>
2025-09-05 17:28:24 -0600

    created index html page, updated app.py to update the list of swimmers names before trying to access individual swimmers

--- first_test_project/webapp/app.py
+++ first_test_project/webapp/app.py
@@ -1,23 +1,34 @@
-from flask import Flask
+from flask import Flask, session, render_template
 import os
 import swimclub
 
 app = Flask(__name__)
+app.secret_key = "You will never guess..."
 
 @app.get("/")
 def index():
-    return "This is a placeholder for your webpage's opening page"
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
+def populate_data():
+    if "swimmers" not in session:
+        swim_files = os.listdir(swimclub.FOLDER)
+        session["swimmers"] = {}
+        for f in swim_files:
+            name, *_ = swimclub.read_swim_data(f)
+            if name not in session["swimmers"]:
+                session["swimmers"][name] = []
+            session["swimmers"][name].append(f)
+
 
 @app.get("/swimmers")
 def display_swimmers():
-    swim_files = os.listdir(swimclub.FOLDER)
-    swimmers = {}
-    for f in swim_files:
-        name, *_ = swimclub.read_swim_data(f)
-        if name not in swimmers:
-            swimmers[name] = []
-        swimmers[name].append(f)
-    return str(sorted(swimmers))
+    populate_data()
+    return str(sorted(session["swimmers"]))
+
+@app.get("/files/<swimmer>")
+def get_swimmers_files(swimmer):
+    populate_data()
+    return str(session["swimmers"][swimmer])
+
+if __name__ == "__main__":
+    app.run(debug=True)
